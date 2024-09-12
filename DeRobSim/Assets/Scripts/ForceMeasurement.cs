19a4e330ad84cbdaa90ceb23eea9da22aa6597c0
unknown <albertozafra7@gmail.com>
2024-09-12 18:16:57 +0200

    Force Measurement Investigation

--- DeRobSim/Assets/Scripts/ForceMeasurement.cs
+++ DeRobSim/Assets/Scripts/ForceMeasurement.cs
@@ -77,6 +77,8 @@ public class ForceMeasurement : MonoBehaviour
 
         mesh = FlexComponent.gameObject.GetComponent<MeshFilter>().mesh;
         meshRenderer = FlexComponent.gameObject.GetComponent<SkinnedMeshRenderer>();
+
+        //ClusterizeMesh(FlexComponent.asset.shapeOffsets.Length);
     }
 
     Dictionary<int, List<int>> ProcessParticles(int start, int end)
@@ -102,6 +104,9 @@ public class ForceMeasurement : MonoBehaviour
         particleData.GetParticles(FlexComponent.GetParticleStartId(), FlexComponent.GetParticleNum(), _particles);
         // ApplyImpulses(particleData); // Assuming ApplyImpulses is part of this class or accessible
 
+        //Debug.LogWarning(FlexComponent.asset.shapeOffsets.Length);
+        
+        //Debug.LogWarning(FlexComponent.m_drawParticlesHelper.m_mesh.subMeshCount);
         // Reset force calculations
         regionForces.Clear();
         regionCenters.Clear();
@@ -114,7 +119,10 @@ public class ForceMeasurement : MonoBehaviour
 
             foreach (var index in region.Value)
             {
+                
                 Vector3 force = particleData.GetVelocity(index) * m_container.damping; // Simplified force calculation
+                if(m_container.damping == 0)
+                    force = particleData.GetVelocity(index);
                 totalForce += force;
                 center += _particles[index - FlexComponent.GetParticleStartId()];
             }
@@ -183,6 +191,7 @@ public class ForceMeasurement : MonoBehaviour
     {
         Vector3[] vertices = mesh.vertices;
         Color[] colors = new Color[vertices.Length];
+
         meshRenderer.sharedMesh = (Mesh) Instantiate(meshRenderer.sharedMesh);
         mesh = meshRenderer.sharedMesh;
 
@@ -214,4 +223,53 @@ public class ForceMeasurement : MonoBehaviour
     {
         return Color.Lerp(Color.green, Color.red, forceMagnitude / (maxForce/2)); // Assuming max force magnitude is big enough to get its half
     }
+
+    void ClusterizeMesh(int n_clusters){
+        // If there is a FlexComponent associated to the script and it has a handle
+        if (FlexComponent && FlexComponent.handle)
+        {
+            // We get the instance of our FlexComponent to access to its particles
+            FlexExt.Instance instance = FlexComponent.handle.instance;
+
+            // We generate an array that will tell us the indices of each particle within the container
+            int[] indices = new int[instance.numParticles];
+            // We copy the previously mentioned indices
+            if (instance.numParticles > 0) FlexUtils.FastCopy(instance.particleIndices, indices);
+
+            if (FlexComponent is FlexSoftActor)
+            {
+                // We generate a Buffer that will contain 
+                m_indexBuffers = new ComputeBuffer[n_clusters];
+                m_particleMaterials = new Material[n_clusters];
+
+                mesh.subMeshCount = n_clusters;
+                for (int i = 0; i < n_clusters; ++i)
+                {
+                    int start = i == 0 ? 0 : FlexComponent.asset.shapeOffsets[i - 1];
+                    int count = FlexComponent.asset.shapeOffsets[i] - start;
+                    mesh.SetIndices(new int[count], MeshTopology.Points, i);
+                    m_indexBuffers[i] = new ComputeBuffer(count, 4);
+                    int[] shape = new int[count];
+                    for (int j = 0; j < count; ++j) shape[j] = indices[FlexComponent.asset.shapeIndices[start + j]];
+                    m_indexBuffers[i].SetData(shape);
+                    m_particleMaterials[i] = new Material(Shader.Find(PARTICLES_SHADER));
+                    m_particleMaterials[i].hideFlags = HideFlags.HideAndDontSave;
+                }
+            }
+
+            mesh.bounds = FlexComponent.bounds;
+
+            MeshFilter meshFilter = GetComponent<MeshFilter>();
+            meshFilter.mesh = mesh;
+
+            meshRenderer.materials = m_particleMaterials;
+            meshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+
+        }
+    }
+
+    Material[] m_particleMaterials;
+    ComputeBuffer[] m_indexBuffers;
+    const string PARTICLES_SHADER = "Flex/FlexDrawParticles";
+
 }
